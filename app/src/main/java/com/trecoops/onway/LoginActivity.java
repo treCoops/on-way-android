1a74068fd815cf2d09f7045a509ac0bf3b547dbb
treCoops <trevogayan1995@gmail.com>
2020-08-24 16:22:43 +0530

    OTP Activity design and Other Changes

--- app/src/main/java/com/trecoops/onway/LoginActivity.java
+++ app/src/main/java/com/trecoops/onway/LoginActivity.java
@@ -2,12 +2,15 @@ package com.trecoops.onway;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.style.ForegroundColorSpan;
 import android.text.style.StyleSpan;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
@@ -19,6 +22,7 @@ public class LoginActivity extends AppCompatActivity {
 
         final TextView txt_main_text = findViewById(R.id.txt_main_text);
         final TextView txt_footer = findViewById(R.id.txt_footer);
+        final Button login_btn_next = findViewById(R.id.login_btn_next);
 
         final String headingString = "Get moving with ONWAY";
         final SpannableString spanHeadingString = new SpannableString(headingString);
@@ -33,6 +37,13 @@ public class LoginActivity extends AppCompatActivity {
         spanFooterString.setSpan(new StyleSpan(Typeface.BOLD),62,76,Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
 
         txt_footer.setText(spanFooterString);
+
+        login_btn_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, OTPActivity.class));
+            }
+        });
 
     }
 }
